f6e6f77b272b9c460fb393b694fdf75ceded9a3c
Vikashchou-dhary <158028747+Vikashchou-dhary@users.noreply.github.com>
2024-02-04 22:10:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,8 +10,8 @@ const location_not_found = document.querySelector('.location-not-found');
 
 const weather_body = document.querySelector('.weather-body');
 async function checkWeather(city){
-    const api_key ="3d797722c2271939cb0d5e62c82e8a58";
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}"
+    const api_key =`3d797722c2271939cb0d5e62c82e8a58`                                                                                        `    ;
+    const url =`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`
 
     const weather_data = await fetch(`${url}`).then(response => response.json());
 
